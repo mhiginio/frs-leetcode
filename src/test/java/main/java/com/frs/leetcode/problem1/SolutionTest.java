49879ba29e4ba07cb1a8382e0a08cebc243c634f
manuel.higinio <manuel.higinio@frs-systems.es>
2024-10-02 15:30:00 +0200

    Prepare repo

---
+++ src/test/java/main/java/com/frs/leetcode/problem1/SolutionTest.java
@@ -0,0 +1,27 @@
+package main.java.com.frs.leetcode.problem1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void test(String s, int result) {
+        assertEquals(result, solution.lengthOfLongestSubstring(s));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+            Arguments.of("abcabcbb", 3),
+            Arguments.of("bbbbbbbbb", 1),
+            Arguments.of("pwwkew", 3)
+        );
+    }
+
+}
---
+++ src/test/java/main/java/com/frs/leetcode/problem2/SolutionTest.java
@@ -0,0 +1,48 @@
+package main.java.com.frs.leetcode.problem2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @ParameterizedTest(name = "{0} should return {1}")
+    @MethodSource("testSolution")
+    void test(char[][] grid, int expected) {
+        assertEquals(expected, solution.numIslands(grid));
+    }
+
+
+    private static Stream<Arguments> testSolution() {
+        return Stream.of(
+            Arguments.of(buildMap("""
+                11110
+                11010
+                11000
+                00000"""), 1),
+            Arguments.of(buildMap("""
+                11000
+                11000
+                00100
+                00011"""), 3));
+    }
+
+
+    private static char[][] buildMap(String s) {
+        String[] split = s.split("\n");
+        int columns = split[0].length();
+        char[][] map = new char[split.length][columns];
+        for (int row = 0; row < split.length; row++) {
+            if (split[row].length() != columns) {
+                throw new IllegalArgumentException("Incorrect map. All rows should have the same sizes");
+            }
+            map[row] = split[row].toCharArray();
+        }
+        return map;
+    }
+}
