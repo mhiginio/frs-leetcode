e82c76f01c039b5a9e8440549e4f372004238563
mhiginio <90598751+mhiginio@users.noreply.github.com>
2024-10-06 16:55:15 +0200

    Creatre readmes and tests (#1)
    Inital commit

---
+++ src/main/java/com/frs/leetcode/problem1/Solution.java
@@ -0,0 +1,7 @@
+package main.java.com.frs.leetcode.problem1;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        return Integer.MIN_VALUE;
+    }
+}
---
+++ src/main/java/com/frs/leetcode/problem2/Solution.java
@@ -0,0 +1,7 @@
+package main.java.com.frs.leetcode.problem2;
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        return Integer.MIN_VALUE;
+    }
+}
