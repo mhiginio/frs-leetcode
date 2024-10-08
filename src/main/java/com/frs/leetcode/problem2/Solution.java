af91723b47877c2861f62626aa6578e0d910a73b
manuel.higinio <manuel.higinio@frs-systems.es>
2024-10-08 15:34:44 +0200

    Prepare repo

--- src/main/java/com/frs/leetcode/problem2/Solution.java
+++ src/main/java/com/frs/leetcode/problem2/Solution.java
@@ -1,11 +1,19 @@
 package main.java.com.frs.leetcode.problem2;
 
 class Solution {
+
+    private static final Movement[] DIRECTIONS = new Movement[]{
+        new Movement(-1, 0),
+        new Movement(1, 0),
+        new Movement(0, 1),
+        new Movement(0, -1)
+    };
+
     public int numIslands(char[][] grid) {
         int result = 0;
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[row].length; col++) {
-                if (grid[row][col] == '1') {
+                if (isLand(grid, row, col)) {
                     result++;
                     fillIsland(grid, row, col);
                 }
@@ -14,14 +22,25 @@ class Solution {
         return result;
     }
 
+    private static boolean isLand(char[][] grid, int row, int col) {
+        return grid[row][col] == '1';
+    }
+
     private void fillIsland(char[][] grid, int row, int col) {
-        if (row < 0 || col < 0 || row == grid.length || col == grid[0].length || grid[row][col] != '1') {
+        if (isOutOfRange(grid, row, col) || !isLand(grid, row, col)) {
             return;
         }
         grid[row][col] = '-';
-        fillIsland(grid, row - 1, col);
-        fillIsland(grid, row + 1, col);
-        fillIsland(grid, row, col - 1);
-        fillIsland(grid, row, col + 1);
+        for (Movement direction : DIRECTIONS) {
+            fillIsland(grid, row + direction.row(), col + direction.column());
+        }
+    }
+
+    private boolean isOutOfRange(char[][] grid, int row, int col) {
+        return row < 0 || col < 0 || row >= grid.length || col >= grid[0].length;
+    }
+
+    public record Movement(int row, int column) {
+
     }
 }
