39a60291f9d1ce179eab31a444463c9850cfdba1
mhiginio <mhiginio@gmail.com>
2024-10-08 06:25:11 +0200

    Problems 2 & 3

--- src/main/java/com/frs/leetcode/problem2/Solution.java
+++ src/main/java/com/frs/leetcode/problem2/Solution.java
@@ -2,6 +2,26 @@ package main.java.com.frs.leetcode.problem2;
 
 class Solution {
     public int numIslands(char[][] grid) {
-        return Integer.MIN_VALUE;
+        int result = 0;
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                if (grid[row][col] == '1') {
+                    result++;
+                    fillIsland(grid, row, col);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void fillIsland(char[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row == grid.length || col == grid[0].length || grid[row][col] != '1') {
+            return;
+        }
+        grid[row][col] = '-';
+        fillIsland(grid, row - 1, col);
+        fillIsland(grid, row + 1, col);
+        fillIsland(grid, row, col - 1);
+        fillIsland(grid, row, col + 1);
     }
 }
--- src/main/java/com/frs/leetcode/problem3/Solution.java
+++ src/main/java/com/frs/leetcode/problem3/Solution.java
@@ -2,6 +2,21 @@ package main.java.com.frs.leetcode.problem3;
 
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        return Integer.MIN_VALUE;
+        int rows = dungeon.length;
+        int columns = dungeon[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int col = columns - 1; col >= 0; col--) {
+            for (int row = rows - 1; row >= 0; row--) {
+                int bestPath = 1;
+                if (row + 1 < rows || col + 1 < columns) {
+                    bestPath = Math.min(
+                            row + 1 < rows ? dp[row + 1][col] : 10000,
+                            col + 1 < columns ? dp[row][col + 1] : 10000);
+                }
+                dp[row][col] = Math.max(1, bestPath - dungeon[row][col]);
+            }
+        }
+
+        return dp[0][0];
     }
 }
