172080d1286ac128a97182c30fe6929d9eab7b7f
manuel.higinio <manuel.higinio@frs-systems.es>
2024-10-10 13:41:16 +0200

    Prepare repo

--- src/main/java/com/frs/leetcode/problem1/Solution.java
+++ src/main/java/com/frs/leetcode/problem1/Solution.java
@@ -1,22 +1,22 @@
 package main.java.com.frs.leetcode.problem1;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
 
     public int lengthOfLongestSubstring(String s) {
-        int[] lastSeen = new int['z' - 'a' + 1];
-        Arrays.fill(lastSeen, -1);
+        Map<Character, Integer> lastSeen = new HashMap<>();
         int left = 0;
         int maxLength = 0;
         for (int right = 0; right < s.length(); right++) {
-            int lastSeenIdx = s.charAt(right) - 'a';
-            if (lastSeen[lastSeenIdx] >= left) {
+            char current = s.charAt(right);
+            if (lastSeen.getOrDefault(current, -1) >= left) {
                 // Update the max length and move left to the next value of the repeated character
                 maxLength = Math.max(maxLength, right - left);
-                left = lastSeen[lastSeenIdx] + 1;
+                left = lastSeen.get(current) + 1;
             }
-            lastSeen[lastSeenIdx] = right;
+            lastSeen.put(current, right);
         }
         return Math.max(maxLength, s.length() - left);
     }
